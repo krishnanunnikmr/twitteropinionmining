11842924a847a31b4896467ee2f20b6a09c64905
user <krishnanunnikmr@gmail.com>
2020-03-14 13:17:38 +0530

    DB changed to h2 db

--- src/main/java/twitter/opinion/mining/app/TweetForm.java
+++ src/main/java/twitter/opinion/mining/app/TweetForm.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.Size;
 
 
 public class TweetForm {
-    @Size(min = 1,max = 255,message = "Tweets must be between 1 and 255 characters")
+    //@Size(min = 1,max = 255,message = "Tweets must be between 1 and 255 characters")
     private String content;
 
     public String getContent() {
